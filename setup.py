8a5340e338bd09dad66fb9c86de47f225c4d1084
JGWill <jgi@jgwill.com>
2024-12-30 11:23:37 -0500

    bump version to 0.2.62 and update dependencies in pyproject.toml, requirements.txt, and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='olca',
-    version = "0.2.60",
+    version = "0.2.62",
     author='Jean GUillaume ISabelle',
     author_email='jgi@jgwill.com',
     description='A Python package for experimenting with Langchain agent and interactivity in Terminal modalities.',

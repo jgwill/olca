e4af8aadcb3d88362c8d3ef4a001d5b692f9217b
JGWill <jgi@jgwill.com>
2025-06-09 15:25:16 -0400

    build

--- _just_build.sh
+++ _just_build.sh
@@ -4,7 +4,7 @@ rm -rf build/ dist/ *.egg-info **/*.egg-info &>/dev/null
 
 
 python setup.py sdist bdist_wheel  && cdist=$(realpath dist/*whl) && \
-	(cd /w/TST/tstolca &&  (conda activate tstolca && (pip uninstall olca -y &>/dev/null;pip install $cdist --quiet )) ) && \
+	(cd /src/testolca &&  (conda activate testolca && (pip uninstall olca -y &>/dev/null;pip install $cdist --quiet )) ) && \
 	echo "Installed $cdist in tstolca" || echo "Failed to install $cdist in tstolca"
 	
 
